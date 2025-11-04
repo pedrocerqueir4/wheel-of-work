d83aeb2506d1c95a86912804a7b5fe350b91faf1
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-04 13:39:00 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -80,7 +80,8 @@ function runSetupCommands() {
     const commands = [
     "bun add canvas-confetti@^1.9.0",
     "bun add @types/canvas-confetti@^1.6.0",
-    "bun add -D eslint-plugin-react"
+    "bun add -D eslint-plugin-react",
+    "bun add eslint-plugin-react"
 ];
     
     if (commands.length === 0) {
